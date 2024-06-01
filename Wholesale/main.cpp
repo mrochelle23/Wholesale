8eeab5d00ea37a3d331045d8fbadf12081c39b9d
mrochelle23 <mrochellebiz@gmail.com>
2024-06-01 11:44:14 -0400

    Initial Commit

---
+++ Wholesale/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Wholesale
+//
+//  Created by Mikhai Rochelle on 6/1/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
