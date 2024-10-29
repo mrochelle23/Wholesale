5e8ad76a990b9491e92a1fd7c2093672ae6a431d
mrochelle23 <mrochellebiz@gmail.com>
2024-10-28 21:49:31 -0400

    Initial Commit

--- Wholesale/main.cpp
+++ Wholesale/main.cpp
@@ -6,9 +6,44 @@
 //
 
 #include <iostream>
+#include <iomanip>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+double calculateRetail(double, double);
+
+int main() 
+    {
+        double wholesale;
+        double markupPercent;
+        double retail;
+        
+        std::cout << "Enter the item's wholesale cost: ";
+        cin >> wholesale;
+    
+        while (wholesale < 0)
+        {
+            std::cout << "Enter a non-negative number";
+            cin >> wholesale;
+        }
+
+        cout << "Enter the item's markup percentage: ";
+        cin >> markupPercent;
+    
+        while (markupPercent < 0)
+        {
+            std::cout << "Enter a non-negative number";
+            cin >> markupPercent;
+        }
+
+        retail = calculateRetail(wholesale, markupPercent);
+        
+        std::cout << "The retail price is $" << setprecision(2)
+                << fixed << retail << endl;
+        return 0;
+}
+
+double calculateRetail (double wholesale, double markupPercent)
+{
+    double markup = wholesale * markupPercent / 100;
+    return wholesale + markup;
 }
